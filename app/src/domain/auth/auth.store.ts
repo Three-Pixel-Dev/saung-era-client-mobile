a6205e448594035af18fa8aed4b68fde9b6771ab
hlyamhtetkyaw <hlan1559@gmail.com>
2025-12-28 20:27:15 +0630

    [ADD] forgot password

---
+++ app/src/domain/auth/auth.store.ts
@@ -0,0 +1,88 @@
+import { create } from "zustand";
+import { authService } from "@/app/src/domain/auth/auth.service";
+import {RegisterPayload} from "@/app/src/domain/auth/auth.types";
+interface AuthStore {
+    user: any | null;
+    isLoading: boolean;
+    error: string | null;
+    tempRegisterData: RegisterPayload | null;
+
+    setLoading: (loading: boolean) => void;
+    setError: (error: string | null) => void;
+
+    requestSignupOtp: (data: RegisterPayload) => Promise<void>;
+    completeRegistration: (otp: string) => Promise<void>;
+}
+
+export const useAuthStore = create<AuthStore>((set, get) => ({
+    user: null,
+    isLoading: false,
+    error: null,
+    tempRegisterData: null,
+
+    setLoading: (loading) => set({ isLoading: loading }),
+    setError: (error) => set({ error }),
+
+    requestSignupOtp: async (data: RegisterPayload) => {
+        set({ isLoading: true, error: null });
+
+        try {
+            await authService.verifyFields(data);
+
+            set({ tempRegisterData: data });
+
+            await authService.requestOtp({
+                phoneNumber: data.phoneNumber,
+                mode: "register"
+            });
+        } catch (err: any) {
+            console.error("OTP Request failed", err);
+            const msg = err.response?.data?.message || err.message || "Request failed.";
+            set({ error: msg });
+            throw err;
+        } finally {
+            set({ isLoading: false });
+        }
+    },
+
+    completeRegistration: async (otp: string) => {
+        const { tempRegisterData } = get();
+
+        if (!tempRegisterData) {
+            const msg = "Registration session expired. Please fill the form again.";
+            set({ error: msg });
+            throw new Error(msg);
+        }
+
+        set({ isLoading: true, error: null });
+
+        try {
+            const verifyResponse = await authService.verifyOtp({
+                phoneNumber: tempRegisterData.phoneNumber,
+                otp: otp,
+                mode: "register"
+            });
+            const { verificationToken } = verifyResponse;
+            if (!verificationToken) {
+                throw new Error("Verification failed: No token received");
+            }
+
+            const registerResponse = await authService.register({
+                ...tempRegisterData,
+                verificationToken: verificationToken,
+            });
+
+            set({
+                user: registerResponse.user,
+                tempRegisterData: null
+            });
+        } catch (err: any) {
+            console.error("Registration failed", err);
+            const msg = err.response?.data?.message || err.message || "Invalid Code or Registration Failed";
+            set({ error: msg });
+            throw err;
+        } finally {
+            set({ isLoading: false });
+        }
+    },
+}));
